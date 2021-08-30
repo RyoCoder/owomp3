c7e7d208db5651ec60233478b8ea73c5389b9b06
OwOBox <89490108+OwOBox@users.noreply.github.com>
2021-08-30 12:11:37 +0700

    Update ytsearch.py

--- handlers/ytsearch.py
+++ handlers/ytsearch.py
@@ -18,13 +18,13 @@ async def ytsearch(_, message: Message):
             await message.reply_text("/search needs an argument!")
             return
         query = message.text.split(None, 1)[1]
-        m = await message.reply_text("🔎 **searching**")
+        m = await message.reply_text("🔎 **Đang tìm kiếm**")
         results = YoutubeSearch(query, max_results=5).to_dict()
         i = 0
         text = ""
         while i < 5:
-            text += f"**Judul:** `{results[i]['title']}`\n"
-            text += f"**Durasi:** {results[i]['duration']}\n"
+            text += f"`{results[i]['title']}`\n"
+            text += f"**Thời lượng:** {results[i]['duration']}\n"
             text += f"**Views:** {results[i]['views']}\n"
             text += f"**Channel:** {results[i]['channel']}\n"
             text += f"https://www.youtube.com{results[i]['url_suffix']}\n\n"
